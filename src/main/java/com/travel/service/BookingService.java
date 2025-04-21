00da8d2059b228ff2ea90728e110e0a67795a767
Pavan Kalluri <pavank839@outlook.com>
2025-04-21 13:57:27 +0530

    Create BookingService.java

---
+++ src/main/java/com/travel/service/BookingService.java
@@ -0,0 +1,17 @@
+package com.travel.service;
+
+import com.travel.model.Booking;
+import com.travel.repository.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+    @Autowired
+    private BookingRepository bookingRepository;
+
+    public Booking createBooking(Booking booking) {
+        return bookingRepository.save(booking);
+    }
+}
